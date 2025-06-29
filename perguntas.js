24b71d43a216d58d0396110e0783d74387500e17
izaloka <izabela.bonjorno.souza@escola.pr.gov.br>
2025-06-29 13:38:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'conhecimentos bàsicos',
+    'quantos continentes existem?',
+    '7 continentes '
+)
+
+criaCartao(
+    'conhecimentos bàsicos',
+    'Qual é o maior planeta do sistema solar?',
+    'Júpiter'
+)
+
+criaCartao(
+    'conhecimentos bàsicos',
+    'Quem foi o autor de "dom casmurro"?',
+    'Machado de Assis '
+)
+
+criaCartao(
+    'conhecimentos bàsicos',
+    'Quantos ossos tem no carpo humano?',
+    '206 ossos.'
+)
